4c4fb45ce17291ccd6211da45e295af0c749ad36
Ed Me <603803799@qq.com>
2017-11-30 09:51:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,8 @@ export default function (option) {
                         break;
                 }
             },
-            {header = !!option.header ? Object.assign(option.header, this.header) : this.header, type = 'GET', url = option, async = true/*, error = this.error*/} = typeof option === 'string' ? {} : option,
+            !!option.header && Object.assign(option.header, this.header),
+            {header=this.header, type = 'GET', url = option, async = true/*, error = this.error*/} = typeof option === 'string' ? {} : option,
             sendURL = `${this.baseURL/*可提前在原型或实例上设置一个项目前缀*/ || ''}${type === 'POST' ? url : !!~url.indexOf('?') ? url + '&timestamp=' + (new Date()).valueOf() : url + '?timestamp=' + (new Date()).valueOf()/*解决IE - GET请求缓存问题*/}`,
             sendData = JSON.stringify(typeof option.data === 'function' ? option.data.call(this) : option.data);
         //this.cancel = xmlHttp.abort;//预留终止请求
