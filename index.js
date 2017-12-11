005cd5370f6e33195aa465f126b55d39622e0b53
Ed Me <603803799@qq.com>
2017-12-10 22:02:29 -0600

    Update index.js

--- index.js
+++ index.js
@@ -57,13 +57,10 @@ export default function (option) {
       sendURL = `${this.baseURL/*可提前在原型或实例上设置一个项目前缀*/ || ''}${!!~type.indexOf('POST') ? url : !!~url.indexOf('?') ? url + '&timestamp=' + (new Date()).valueOf() : url + '?timestamp=' + (new Date()).valueOf()/*解决IE - GET请求缓存问题*/}`,
       sendData = JSON.stringify(typeof option.data === 'function' ? option.data.call(this) : option.data);
     //this.cancel = xmlHttp.abort;//预留终止请求
-    xmlHttp.onreadystatechange = state_Change;
     //xmlHttp.onerror = e => console.error(e);//预留捕获异常、错误
-    xmlHttp.open(type, sendURL, async);
-    header && (() => { // 设置请求头部，默认取原型或实例上的header
+    [xmlHttp.onreadystatechange] = [state_Change, xmlHttp.open(type, sendURL, async), header && (() => { // 设置请求头部，默认取原型或实例上的header
       for (let [k, v] of Object.entries(header)) xmlHttp.setRequestHeader(k, v);
-    })();
-    xmlHttp.send(!!~type.indexOf('POST') ? this.encode ? this.encode(sendData) : sendData : null)
+    })(), xmlHttp.send(!!~type.indexOf('POST') ? this.encode ? this.encode(sendData) : sendData : null)];
   });
 };
 
